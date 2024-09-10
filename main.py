28a3deb0923de2492815c801cbf5a6baa57f55cc
hasderhi <hasederhi.12@gmail.com>
2024-09-10 16:25:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+from tkinter import font
+from tkinter import messagebox
+from tkinter import PhotoImage
+
+
+neuesFenster = Tk()
+neuesFenster.title("Name des Fensters") #Hier geben wir den Namen des Fensters ein
+
+
+neuesFenster.mainloop()
+
+
